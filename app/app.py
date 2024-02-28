2c3558c4076f850510ed0ff35c1ba41923ca47b4
Lorenaaa11 <jenifer20lorena@gmail.com>
2024-02-28 09:48:36 -0500

    FLASK

--- app/app.py
+++ app/app.py
@@ -1,17 +1,40 @@
-from flask import *
+from flask import*  #flask,render_template
+import mysql.connector
 
 app = Flask (__name__)
+
+db = mysql.connector.connect(
+        host="localhost",
+    user="root",
+    password="",
+    database="Agenda2024"
+)
+curso = db.cursor()
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/Registrar')
-def saludos():
-    return render_template('Registrar.html')
+@app.route('/Registrar', methods=['POST'])
+def registrar_usuario():
+    Nombre = request.form['nombre'],
+    Apellido = request.form['apellido'],
+    Email = request.form['email'],
+    Direccion = request.form['direccion'],
+    Telefono = request.form['telefono'],
+    Usuario = request.form['usuario'],
+    Contraseña = request.form['contrasena']
+
+
+    curso.execute("INSERT INTO personas(Nombreper,Apellidoper,Emailper,direccionper,Telefono,Usuarioper,Contraseñaper)VALUES(%s,%s,%s,%s,%s,%s,%s)",(Nombre,Apellido,Email,Direccion,Telefono,Usuario,Contraseña))
+    db.commit()
+
+
+    return redirect(url_for('Registrar'))
 
 if __name__ == '__main__':
     app.add_url_rule('/',view_func=index)
     app.run(debug=True,port=5005)
+     
 
   
