7d0af07ae77411688b8a45e75b49628fc745d7b9
Lorenaaa11 <jenifer20lorena@gmail.com>
2024-03-01 09:43:19 -0500

    FLASK

--- app/app.py
+++ app/app.py
@@ -1,10 +1,10 @@
-from flask import*  #flask,render_template
+from flask import Flask,render_template,request,redirect,url_for
 import mysql.connector
 
 app = Flask (__name__)
 
 db = mysql.connector.connect(
-        host="localhost",
+    host="localhost",
     user="root",
     password="",
     database="Agenda2024"
@@ -15,26 +15,28 @@ curso = db.cursor()
 def index():
     return render_template('index.html')
 
-@app.route('/Registrar', methods=['POST'])
+@app.route('/Registrar', methods=['GET','POST'])
 def registrar_usuario():
-    Nombre = request.form['nombre'],
-    Apellido = request.form['apellido'],
-    Email = request.form['email'],
-    Direccion = request.form['direccion'],
-    Telefono = request.form['telefono'],
-    Usuario = request.form['usuario'],
-    Contraseña = request.form['contrasena']
+    if request.method == 'POST':
+        Nombre = request.form.get('nombre')
+        Apellido = request.form.get('apellido')
+        Email = request.form.get('email')
+        Direccion = request.form.get('direccion')
+        Telefono = request.form.get('telefono')
+        Usuario = request.form.get('usuario')
+        Contraseña = request.form.get('Contrasena')
 
 
-    curso.execute("INSERT INTO personas(Nombreper,Apellidoper,Emailper,direccionper,Telefono,Usuarioper,Contraseñaper)VALUES(%s,%s,%s,%s,%s,%s,%s)",(Nombre,Apellido,Email,Direccion,Telefono,Usuario,Contraseña))
-    db.commit()
+        curso.execute("INSERT INTO personas(Nombreper,Apellidoper,Emailper,direccionper,Telefono,Usuarioper,Contraseñaper)VALUES(%s,%s,%s,%s,%s,%s,%s)",(Nombre,Apellido,Email,Direccion,Telefono,Usuario,Contraseña))
+        db.commit()
 
 
-    return redirect(url_for('Registrar'))
+        return redirect(url_for('registrar_usuario'))
+    return render_template('Registrar.html')
+
 
 if __name__ == '__main__':
     app.add_url_rule('/',view_func=index)
     app.run(debug=True,port=5005)
-     
 
   
